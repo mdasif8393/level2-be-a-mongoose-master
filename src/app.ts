0e94adbe9525ec40634b6aea832e57e0e7435b7b
mdasif8393 <md.asif.8393@gmail.com>
2023-06-19 10:58:33 +0600

    add

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/user',userRoutes);
 
 export default app;
